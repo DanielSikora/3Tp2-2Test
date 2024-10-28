784f72a6ec1e12313adb6489a1001ae6b0b9eae5
Pijo <danielsikora100@gmail.com>
2024-10-28 09:42:10 +0100

    first commit

---
+++ hello.js
@@ -0,0 +1,10 @@
+// console.log("Hello, world!");
+const fs = require('fs');
+
+fs.readFile("example.txt", 'utf-8', (err, data) => {
+    if (err) {
+        console.log('Błąd podczas odczytu pliku: ', err);
+    } else {
+        console.log(data);
+    }
+});
